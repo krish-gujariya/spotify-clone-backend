d0bbfb9ba1c808f2b100fedd30ab07e9a8ad9cde
krish.gujariya <krish.gujariya.2024@gmail.com>
2024-05-28 12:32:17 +0530

    initalize prisma setup and seeder files

---
+++ prisma/seeders/seed.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { logger } from "../../src/utils/pino";
+
+
+const genPassword = async(passString:string)=>{
+    return await bcrypt.hash(passString,10);
+}
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+    try {
+         await prisma.users.upsert({
+            where:{email:"krish@gmail.com"},
+            update:{},
+            create:{
+                    email:"krish@gmail.com",
+                    name:"Krish Gujariya",
+                    password: await genPassword("mypassword1"),
+                    dob: new Date("12-03-2003"),
+            }
+        })
+
+        await prisma.genres.upsert({
+            where:{id:1},
+            update:{},
+            create:{
+                id:1,
+                name:'Indie',
+            }
+        })
+
+        await prisma.artists.upsert({
+            where:{id:1},
+            update:{},
+            create:{
+                name: "Rochak Kohli",
+                genre:1,
+            }   
+            
+        })
+
+    } catch (error) {
+        logger.info(error);
+        
+    }
+}
+
+main();
